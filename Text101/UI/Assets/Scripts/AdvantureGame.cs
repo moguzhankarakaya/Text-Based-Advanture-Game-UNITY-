75253839ea39f8c8b2169633181fe8e6713fabf9
M. Oguzhan <moguzhankarakaya@gmail.com>
2019-10-27 02:11:16 +0200

    Core mechanics is implemented. Stroy is reaming

---
+++ Text101/UI/Assets/Scripts/AdvantureGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdvantureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    
+    State state;
+    KeyCode pressedKey;
+
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        if (DetectInput())
+        {
+            var nextState = state.GetNextState(pressedKey);
+            state = nextState;
+            textComponent.text = state.GetStateStory();
+        }
+    }
+
+    public bool DetectInput()
+    {
+        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(kcode))
+            {
+                pressedKey = kcode;
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Text101/UI/Assets/Scripts/State.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{   
+    //First int for TextArea is the minimum height of the 
+    //TextArea that will be attached to SerializedField in
+    //Unity Inspector area. The second number is the maximum
+    //height for the expension before scrolling starts.
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+    [SerializeField] State   prevSteate;
+    [SerializeField] KeyCode stateStartKey;
+
+    public string GetStateStory() { return storyText; }
+    public KeyCode GetSateStartKey() { return stateStartKey; }
+
+    public State GetNextState(KeyCode userInput)
+    {
+        if (userInput == KeyCode.Backspace)
+        {
+            return prevSteate;
+        }
+        foreach (var state in nextStates)
+        {
+            if (userInput == state.GetSateStartKey())
+                return state;
+        }
+        return this;
+    }
+}
